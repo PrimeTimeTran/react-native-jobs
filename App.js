54a89ecbb1654e682552c0c4b128820e9abaf235
Loi Tran <lvt15@my.fsu.edu>
2018-05-20 13:39:35 +0800

    Add Push notification for client

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
+
+import { Notifications } from 'expo';
+import registerForNotifications from './services/push_notifications'
 
 import store from './store';
 
@@ -10,6 +13,21 @@ import RootNavigation from './navigation/RootNavigation';
 console.disableYellowBox = true;
 
 class App extends React.Component {
+  componentDidMount() {
+    registerForNotifications();
+    Notifications.addListener((notification) => {
+      const { data: { text }, origin } = notification;
+
+      if (origin === 'received' && text ) {
+        Alert.alert(
+          'New Push Notification',
+          text,
+          [{ text: 'Ok.' }]
+        )
+      }
+    })
+  }
+
   render() {
     return (
       <Provider store={store}>
